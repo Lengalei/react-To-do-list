16884c8e97f4f420cc543083a49a9ec1ca0051eb
Lengalei <ruthlengalei@gmail.com>
2024-04-23 23:23:53 +0300

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ export default function App(){
   const[todos, setTodos] = useState("[]")
   function handleSubmit(e){
     e.preventDefault()
-  }
+
+    setTodos(currentTodos => {
+      return[
+        ...currentTodos, {id: crypto.randomUUID(), title: newItem, completed: false},
+      ]
+    })
+}
   return (
   <>
   <form onClick={handleSubmit}  className="new-item-form">
@@ -18,22 +24,18 @@ export default function App(){
   </form>
   <h1 className="header">To Do list</h1>
   <ul className="list">
-    <li>
-      <label>
-        <input type="checkbox"/>
-        Item 1
-      </label>
-      <button className="btn btn-danger">Delete</button>
-      
-    </li>
-    <li>
-      <label>
-        <input type="checkbox"/>
-        Item 2
-      </label>
-      <button className="btn btn-danger">Delete</button>
-
-    </li>
+    {todos.map (todo => {
+      return (
+      <li>
+        <label>
+          <input type="checkbox" checked={todo.completed}/>
+          {todo.title}
+        </label>
+        <button className="btn btn-danger">Delete</button>
+        
+      </li>
+      ) 
+})}
   </ul>
   </>
   )
